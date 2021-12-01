2e2472252ac071d495849d9ec6b7909c41930a4e
santiago-garabaya <santiago.garabaya@gmail.com>
2021-12-01 18:06:21 +0100

    Initial commit.

--- src/day11.zig
+++ src/day11.zig
@@ -1,44 +1,13 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
-const List = std.ArrayList;
-const Map = std.AutoHashMap;
-const StrMap = std.StringHashMap;
-const BitSet = std.DynamicBitSet;
-const Str = []const u8;
 
-const util = @import("util.zig");
-const gpa = util.gpa;
+fn part1(allocator: *Allocator) !void {}
 
-const data = @embedFile("../data/day11.txt");
+fn part2(allocator: *Allocator) !void {}
 
 pub fn main() !void {
-    
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+
+    try part1(&gpa.allocator);
+    try part2(&gpa.allocator);
 }
-
-// Useful stdlib functions
-const tokenize = std.mem.tokenize;
-const split = std.mem.split;
-const indexOf = std.mem.indexOfScalar;
-const indexOfAny = std.mem.indexOfAny;
-const indexOfStr = std.mem.indexOfPosLinear;
-const lastIndexOf = std.mem.lastIndexOfScalar;
-const lastIndexOfAny = std.mem.lastIndexOfAny;
-const lastIndexOfStr = std.mem.lastIndexOfLinear;
-const trim = std.mem.trim;
-const sliceMin = std.mem.min;
-const sliceMax = std.mem.max;
-
-const parseInt = std.fmt.parseInt;
-const parseFloat = std.fmt.parseFloat;
-
-const min = std.math.min;
-const min3 = std.math.min3;
-const max = std.math.max;
-const max3 = std.math.max3;
-
-const print = std.debug.print;
-const assert = std.debug.assert;
-
-const sort = std.sort.sort;
-const asc = std.sort.asc;
-const desc = std.sort.desc;
