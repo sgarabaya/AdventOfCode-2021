47608776818dd46975cca0e809bbe505867f87c0
santiago-garabaya <santiago.garabaya@gmail.com>
2021-12-02 11:20:21 +0100

    Day 02

--- src/day02.zig
+++ src/day02.zig
@@ -1,13 +1,84 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
+const print = std.debug.print;
 
-fn part1(allocator: *Allocator) !void {}
+const data = @embedFile("../data/day02.txt");
 
-fn part2(allocator: *Allocator) !void {}
+const Verb = enum {
+    Forward,
+    Down,
+    Up,
+};
+
+const Command = struct {
+    verb: Verb,
+    amount: u32,
+
+    pub fn from(line: []const u8) !Command {
+        var splitter = std.mem.tokenize(line, " ");
+
+        var verbString = splitter.next().?;
+        var amountString = splitter.next().?;
+
+        var verb = switch (verbString[0]) {
+            'f' => Verb.Forward,
+            'd' => Verb.Down,
+            'u' => Verb.Up,
+            else => @panic("Oops! Wrong verb?"),
+        };
+
+        var amount = try std.fmt.parseUnsigned(u32, amountString, 10);
+
+        return Command{
+            .verb = verb,
+            .amount = amount,
+        };
+    }
+};
+
+fn part1() !void {
+    var lineTokenizer = std.mem.tokenize(data, "\r\n");
+
+    var horizontal: u32 = 0;
+    var depth: u32 = 0;
+
+    while (lineTokenizer.next()) |line| {
+        var command = try Command.from(line);
+
+        switch (command.verb) {
+            .Forward => horizontal += command.amount,
+            .Down => depth += command.amount,
+            .Up => depth -= command.amount,
+        }
+    }
+
+    print("Part1: Horizontal: {d}, vertical: {d} => Product: {d}\n", .{ horizontal, depth, horizontal * depth });
+}
+
+fn part2() !void {
+    var lineTokenizer = std.mem.tokenize(data, "\r\n");
+
+    var aim: u32 = 0;
+    var horizontal: u32 = 0;
+    var depth: u32 = 0;
+
+    while (lineTokenizer.next()) |line| {
+        var command = try Command.from(line);
+
+        switch (command.verb) {
+            .Forward => {
+                horizontal += command.amount;
+                depth += command.amount * aim;
+            },
+            .Down => aim += command.amount,
+            .Up => aim -= command.amount,
+        }
+    }
+
+    print("Part2: Horizontal: {d}, vertical: {d} => Product: {d}\n", .{ horizontal, depth, horizontal * depth });
+}
 
 pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-
-    try part1(&gpa.allocator);
-    try part2(&gpa.allocator);
+    try part1();
+    try part2();
 }
